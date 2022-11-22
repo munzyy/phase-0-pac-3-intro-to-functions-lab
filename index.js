82482860d7142c8bf0995fe44b29efb0123a8e95
Ryan Munz <ryan.munzy@gmail.com>
2022-11-22 01:10:03 -0500

    commit tree d877fe03cd81a51b04fef50a808ce0466d6f88ad

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+
+function logShout(string) {
+    console.log (string.toUpperCase());
+}
+    
+function logWhisper(string) {
+    console.log (string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+
+    console.log(string)
+    console.log(string.toUpperCase());
+
+    console.log(string === string.toUpperCase());
+
+    
+
+   
+
+
+}
